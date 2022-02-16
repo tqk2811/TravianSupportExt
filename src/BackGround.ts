acedc8cc984764a31270d7bf5d99e6541f682995
tqk2811 <tqk2811@gmail.com>
2022-02-16 16:31:54 +0700

    firefox extension

--- src/BackGround.ts
+++ src/BackGround.ts
@@ -1,16 +1,13 @@
-chrome.runtime.onStartup.addListener(function() {
-    console.log("start");
-});
-
-chrome.browserAction.onClicked.addListener(function(tab : chrome.tabs.Tab) {
-    chrome.tabs.create({url: "options.html"});
-});
-
-chrome.runtime.onMessage.addListener(function (message, sender, callback) {
-	if (message){
-        if (message == "inject_resource_githubio"){
-            let is_cb_inject_resource = localStorage.getItem('inject_resource_githubio');
-            callback(is_cb_inject_resource == "true");
-        }
-    }
-});
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details : chrome.webRequest.WebRequestBodyDetails) : chrome.webRequest.BlockingResponse {
+        let resut : chrome.webRequest.BlockingResponse = {
+            cancel: true,
+        };
+        return resut;
+    },
+    {
+        types: ["script"],
+        urls: ["*://*.consensu.org/*"]
+    },
+    ["blocking"]
+);
--- src/Inject.ts
+++ src/Inject.ts
@@ -40,19 +40,9 @@ let scripts = [
     
 ];
 
-
-let is_inject_githubio = chrome.runtime.sendMessage("inject_resource_githubio",function(is_cb_inject_resource: boolean){
-    console.log("is_cb_inject_resource: " + is_cb_inject_resource);
-    let path = chrome.runtime.getURL("");
-    if(is_cb_inject_resource == true) path = "https://tqk2811.github.io/TravianSupportExt/";
-
-    localStorage.setItem("TSRoot",path);
-    AddUriCss(path + "libs/css/jquery.dataTables.min.css");
-    AddUriCss(path + "libs/css/rowReorder.dataTables.min.css");
-    AddUriCss(path + "css/TS.css");
-    for(let i = 0; i < scripts.length; i++) AddUriScript(path + scripts[i]);
-});
-
-
-
-
+let path = chrome.runtime.getURL("");
+localStorage.setItem("TSRoot",path);
+AddUriCss(path + "libs/css/jquery.dataTables.min.css");
+AddUriCss(path + "libs/css/rowReorder.dataTables.min.css");
+AddUriCss(path + "css/TS.css");
+for(let i = 0; i < scripts.length; i++) AddUriScript(path + scripts[i]);
