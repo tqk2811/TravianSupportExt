f7a1f3736832434aaed6f436f52797bdc6f08783
tqk2811 <tqk2811@gmail.com>
2022-09-23 01:52:18 +0700

    add default LinkedList

--- src/Injects/DataManagers/AccountData.ts
+++ src/Injects/DataManagers/AccountData.ts
@@ -32,7 +32,11 @@ class AccountData implements IAccountData {
             UserName: userName,
             LinkedList: [
                 { Name: "Farm List", Url: "/build.php?id=39&gid=16&tt=99", openNewTab: false },
-                { Name: "Green Attack Report", Url: "/report/offensive?opt=AAABAA==", openNewTab: false },
+                { Name: "Green Attack Reports", Url: "/report/offensive?opt=AAABAA==", openNewTab: false },
+                { Name: "Alli Attack Reports", Url: "/alliance/reports?filter=1,2,3&own=0&page=1", openNewTab: false },
+                { Name: "Alli Defend Reports", Url: "/alliance/reports?filter=4,5,6,7&own=0&page=1", openNewTab: false },
+                { Name: "Alli Spy Reports", Url: "/alliance/reports?filter=15,16,17&own=0&page=1", openNewTab: false },
+                { Name: "Alli Def-Spy Reports", Url: "/alliance/reports?filter=18,19&own=0&page=1", openNewTab: false },
             ],
             CheckboxData: {},
             VillageAdvanced: VillageAdvanced.Build
